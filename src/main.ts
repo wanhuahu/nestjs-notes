9fdda56bb007ec7c84dd795a991afa4ac90ee04a
Wanhua Hu <wanhuahu@gmail.com>
2025-03-21 12:40:22 -0400

    port 3001

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors(); // Enable CORS
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
